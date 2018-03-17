313b9b07bc5a5f86b936128165c04cb56b681c86
KGBcomrade <vlasov.sviatoslav@gmail.com>
2018-03-17 16:01:31 +0300

    Tyt kakoy-to kal byl
    mda

--- Predator.cpp
+++ Predator.cpp
@@ -11,7 +11,6 @@ Predator::Predator()
 Predator::~Predator() = default;
 
 void Predator::eat(Animal *a) {
-<<<<<<< HEAD
 	if (!alive)
 		cout << name << " is dead." << endl;
 	else if (!a->alive)
@@ -21,8 +20,4 @@ void Predator::eat(Animal *a) {
 		cout << name << " ate " << a->name << '.' << endl;
 	}
 	
-=======
-	health += a->getSaturity();
-	delete a;
->>>>>>> 52f7bda0d4aa9aebdbe2ecbb4c87cdd1e0b84171
 }
